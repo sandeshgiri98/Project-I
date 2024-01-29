f4f1a2b02e62f85bafc66c63ca9ccade7ddc9dd6
sandeshgiri98 <sandeshgiri022@gmail.com>
2024-01-29 21:39:29 +0545

    redirect

---
+++ profile_a/redirect.php
@@ -0,0 +1,36 @@
+<?php
+include_once('../dashboard_a/admin_dash.php');
+include_once('../register/connection.php');
+
+// Check if the form was submitted
+if (isset($_POST['submit'])) {
+    $image = $_FILES['image']['name'];
+
+    //target directory and file path
+    $target_directory = "../photoes/";
+    $target_file = $target_directory . $image;
+
+    // Move the uploaded file to the target directory
+    if (move_uploaded_file($_FILES['image']['tmp_name'], $target_file)) {
+
+        // Update the profile_image column in the database
+        
+        $updateQuery = "UPDATE register SET profile_image='$image' WHERE fake_user = ?";
+        $stmt = $con->prepare($updateQuery);
+        $stmt->bind_param("s", $_SESSION['fake_user']);
+
+        if ($stmt->execute()) {
+            echo "<script>alert('Profile Picture Added Successfully!'); window.location.href='../profile_a/profile_setting.php';</script>";
+            exit();
+        } else {
+            echo "<script>alert('Failed to Upload.'); window.location.href='../profile_a/profile_setting.php';</script>";
+        }
+
+        $stmt->close();
+    } else {
+        echo "<script>alert('Error moving uploaded file.'); window.location.href='../profile_a/profile_setting.php';</script>";
+    }
+}
+
+$con->close();
+?>
